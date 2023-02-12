c810cd7b67358dabd900ff2663e67a6370a4f177
DanVi007 <danvitrinh@outlook.com>
2023-02-12 15:56:53 +0100

    lessen redundancy

--- university_service/handlers/uniInfoHandler.go
+++ university_service/handlers/uniInfoHandler.go
@@ -7,52 +7,85 @@ import (
 	"net/http"
 )
 
-func UniInfoHandler(w http.ResponseWriter, r *http.Request) {
-	uniUrl := "http://" + UniversitiesAPIurl + UniversitiesSearch
-
-	//TODO: consider using get instead
-	// Create new request
-	uniRequest, err := http.NewRequest(http.MethodGet,
-		uniUrl, nil)
+// TODO: consider using get instead
+func getResponseFromApi(w http.ResponseWriter,
+	apiURL, apiName string) *http.Response {
+	request, err := http.NewRequest(http.MethodGet, apiURL, nil)
 	if err != nil {
-		http.Error(w, "Error in creating new request to universities API",
+		http.Error(w, "Error in creating new request to "+apiName+" API",
 			http.StatusInternalServerError)
-		return
+		return nil
 	}
 
-	uniRequest.Header.Add("content-type", "application/json")
+	// request.Header.Add("content-type", "application/json")
 
 	client := &http.Client{}
 	defer client.CloseIdleConnections()
 
-	res, err := client.Do(uniRequest)
+	res, err := client.Do(request)
 
 	if err != nil {
-		http.Error(w, "Error in getting response from universities API",
+		http.Error(w, "Error in getting response from "+apiName+" API",
 			http.StatusInternalServerError)
-		return
+		return nil
 	}
+	return res
+}
+
+func populateDataWithResponse(w http.ResponseWriter, res *http.Response,
+	uniName string, data interface{}) bool {
 
 	body, err := ioutil.ReadAll(res.Body)
+
 	if err != nil {
 		http.Error(w, "Error during reading response",
 			http.StatusInternalServerError)
-		return
+		return false
 	}
+
+	err = json.Unmarshal(body, &data)
+
+	if err != nil {
+		http.Error(w, "Error during unmarshaling from "+uniName+" API",
+			http.StatusInternalServerError)
+		return false
+	}
+	return true
+}
+
+func UniInfoHandler(w http.ResponseWriter, r *http.Request) {
+	uniUrl := "http://" + UniversitiesAPIurl + UniversitiesSearch
+	uniName := "university"
 
 	var unisFound []University
 
-	err = json.Unmarshal(body, &unisFound)
-
-	if err != nil {
-		http.Error(w, "Error during unmarshaling from universities API",
-			http.StatusInternalServerError)
+	res := getResponseFromApi(w, uniUrl, uniName)
+	if res == nil {
 		return
 	}
 
+	if !populateDataWithResponse(w, res, uniName, &unisFound) {
+		return
+	}
+	// body, err := ioutil.ReadAll(res.Body)
+
+	// if err != nil {
+	// 	http.Error(w, "Error during reading response",
+	// 		http.StatusInternalServerError)
+	// 	return
+	// }
+
+	// err = json.Unmarshal(body, &unisFound)
+
+	// if err != nil {
+	// 	http.Error(w, "Error during unmarshaling from "+uniName+" API",
+	// 		http.StatusInternalServerError)
+	// 	return
+	// }
+
 	w.Header().Add("content-type", "application/json")
 
-	_, err = fmt.Fprintln(w, unisFound[0])
+	_, err := fmt.Fprintln(w, unisFound[0])
 	if err != nil {
 		http.Error(w, "Error during returning output",
 			http.StatusInternalServerError)
